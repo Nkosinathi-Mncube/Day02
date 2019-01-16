85a313d10f851cfa4dd43a2b07363f91c1396ef0
Nkosi-nathi <mncubejames.com>
2019-01-16 21:57:26 +0200

    correct version

--- ex03/ft_is_negetive.c
+++ ex03/ft_is_negetive.c
@@ -1,21 +1,20 @@
 #include <unistd.h>
+
 void ft_putchar(char c)
 {
 	write(1,&c,1);
 }
-
 void ft_is_negetive(int n)
 {
-	if (n=='\0')
+	if (n<0)
 	{
-		ft_putchar('p');
+		ft_putchar('N');
 	}
 	else
 	{
-		
+		if ((n>=0)||(n=='\0'))
+		{
+			ft_putchar('P');
+		}
 	}	
-}
-int main()
-{
-	return 0;
 }
